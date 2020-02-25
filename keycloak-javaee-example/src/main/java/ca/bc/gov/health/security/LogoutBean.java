6f9d74ebe4dbab1067b66763156f95748d2a0be3
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-02-25 11:01:25 -0800

    formatting

--- keycloak-javaee-example/src/main/java/ca/bc/gov/health/security/LogoutBean.java
+++ keycloak-javaee-example/src/main/java/ca/bc/gov/health/security/LogoutBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.bc.gov.health.security;
 
 import java.io.IOException;
@@ -14,22 +9,23 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 /**
- * Request scoped bean with a method to log the user out of their current Keycloak and application sessions
+ * Request scoped bean with a method to log the user out of their current
+ * Keycloak and application sessions
  */
 @RequestScoped
 @Named("LogoutBean")
 public class LogoutBean implements Serializable {
-    
+
     public void logout() throws UnsupportedEncodingException, IOException {
-        
-       //Call the keycloak realm end-session endpoint to delete the session from keycloak
-       FacesContext.getCurrentInstance()
-               .getExternalContext()
-               .redirect("https://localhost:8543/auth/realms/moh-users-realm/protocol/openid-connect/logout?redirect_uri=" 
-                       + URLEncoder.encode("https://localhost:8181/keycloak-javaee-example/callback", "UTF-8"));
-       
-       //Invalidate the user browser session
-       FacesContext.getCurrentInstance().getExternalContext().invalidateSession(); 
+
+        //Call the keycloak realm end-session endpoint to delete the session from keycloak
+        FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .redirect("https://localhost:8543/auth/realms/moh-users-realm/protocol/openid-connect/logout?redirect_uri="
+                        + URLEncoder.encode("https://localhost:8181/keycloak-javaee-example/callback", "UTF-8"));
+
+        //Invalidate the user browser session
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
     }
-   
+
 }
