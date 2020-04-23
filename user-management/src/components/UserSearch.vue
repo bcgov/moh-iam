8b3a519952f35378937ca542d8af37a377718daa
David Sharpe <david.a.sharpe@cgi.com>
2020-04-23 16:23:17 -0700

    AB#491 Add enter key handling for Search and some extra columns for search results.

--- user-management/src/components/UserSearch.vue
+++ user-management/src/components/UserSearch.vue
@@ -1,70 +1,70 @@
 <template>
-  <div>
-    <div class="col1">
-      <v-text-field outlined dense v-model="userSearchInput" placeholder="Search for user" />
-      <v-btn class="secondary" small v-on:click="searchUser">Search Users</v-btn>
+    <div>
+        <div class="col1">
+            <v-text-field outlined dense label="Search" v-model="userSearchInput"
+                          placeholder="Username, email, name, or ID"
+                          @keyup.native.enter="searchUser"/>
+            <v-btn class="secondary" small v-on:click="searchUser">Search Users</v-btn>
+        </div>
+        <div class="col4">
+            <v-data-table
+                    :headers="headers"
+                    :items="searchResults"
+                    :items-per-page="5"
+                    class="elevation-1"
+            ></v-data-table>
+        </div>
     </div>
-    <div class="col4">
-      <v-data-table
-        :headers="headers"
-        :items="searchResults"
-        :items-per-page="5"
-        class="elevation-1"
-      ></v-data-table>
-    </div>
-  </div>
 </template>
 
 <script>
-import axios from "axios";
+    import axios from "axios";
 
-export default {
-  name: "UserSearch",
+    export default {
+        name: "UserSearch",
 
-  data() {
-    return {
-      headers: [
-        {
-          text: "ID",
-          align: "start",
-          sortable: false,
-          value: "id"
+        data() {
+            return {
+                headers: [
+                    {text: "Username", value: "username"},
+                    {text: "First name", value: "firstName"},
+                    {text: "Last name", value: "lastName"},
+                    {text: "Email", value: "email"},
+                    {text: "Enabled", value: "enabled"},
+                    {text: "ID", value: "id"}
+                ],
+                result: "",
+                userSearchInput: "",
+                searchResults: [],
+                clients: [],
+            };
         },
-        { text: "Username", value: "username" }
-      ],
-      result: "",
-      userSearchInput: "",
-      searchResults: [],
-      clients: [],
-      checkbox1: false
-    };
-  },
-  methods: {
-    searchUser: function() {
-      var vm = this;
-      this.$keycloak.updateToken().success(function() {
-        var url =
-          vm.$keycloak.authServerUrl +
-          "admin/realms/" +
-          vm.$keycloak.realm +
-          "/users?briefRepresentation=true&first=0&max=20&search=" +
-          vm.userSearchInput;
+        methods: {
+            searchUser: function () {
+                var vm = this;
+                this.$keycloak.updateToken().success(function () {
+                    var url =
+                    vm.$keycloak.authServerUrl +
+                    "admin/realms/" +
+                    vm.$keycloak.realm +
+                    "/users?briefRepresentation=true&first=0&max=20&search=" +
+                    vm.userSearchInput;
 
-        axios
-          .get(url, {
-            headers: { Authorization: "Bearer " + vm.$keycloak.token }
-          })
-          .then(response => {
-            vm.result = response.data;
-            vm.searchResults = response.data;
-          })
-          .catch(e => {
-            vm.result = e;
-          });
-      });
-    }
-  }
-};
+                    axios
+                    .get(url, {
+                        headers: {Authorization: "Bearer " + vm.$keycloak.token}
+                    })
+                    .then(response => {
+                        vm.result = response.data;
+                        vm.searchResults = response.data;
+                    })
+                    .catch(e => {
+                        vm.result = e;
+                    });
+                });
+            }
+        }
+    };
 </script>
 
 <style scoped>
