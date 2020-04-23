0bc2c00e9f557cbf87f96ac5af203de7f52e804b
Trevor Schiavone <trevor.schiavone@cgi.com>
2020-04-23 13:17:51 -0700

    added veutify theming

--- user-management/src/plugins/vuetify.js
+++ user-management/src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#003366',
+                secondary: '#38598a'
+            },
+        },
+    },
 });
