c7019b62102302309ee27eb457a1fa6028fa25ad
David Sharpe <david.a.sharpe@cgi.com>
2020-04-24 22:26:46 -0700

    AB#481 Fix Travis CI build.

--- user-management/src/App.vue
+++ user-management/src/App.vue
@@ -11,7 +11,7 @@
 
         <div class="col4">
           <v-checkbox v-model="showKeycloakTools" :label="`Keycloak Dev Tools`"></v-checkbox>
-          <KeycloakDevTools v-show="showKeycloakTools"
+          <KeycloakDevTools v-show="showKeycloakTools" />
         </div>
       </section>
     </main>
