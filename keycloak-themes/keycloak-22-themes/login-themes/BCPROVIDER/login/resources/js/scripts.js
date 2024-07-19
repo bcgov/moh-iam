93c4cec7c736146072151087592bcb2ee7e9ac04
filipflorek <filip.florek@cgi.com>
2024-07-19 14:46:13 -0600

    generate login themes

---
+++ keycloak-themes/keycloak-22-themes/login-themes/BCPROVIDER/login/resources/js/scripts.js
@@ -0,0 +1,28 @@
+const IDPS_TO_SHOW = ["bcprovider_aad"];
+
+function hideIdentityProviders(allIdentityProviders) {
+    allIdentityProviders.forEach(idpAlias => {
+        if(IDPS_TO_SHOW.indexOf(idpAlias) === -1){
+            document.getElementById('zocial-' + idpAlias).style.display = 'none';
+        }
+    })
+}
+
+// When the user clicks on the button, open the modal
+function showInstructionsModal() {
+    var modal = document.getElementById("instructions-modal");
+    modal.style.display = "block";
+};
+
+function hideInstructionsModal() {
+    var modal = document.getElementById("instructions-modal");
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    var modal = document.getElementById("instructions-modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
